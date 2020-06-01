a2a0d53a115ccafa17cd5f56f9600923917a06ca
iamzhangqing <736668941@qq.com>
2020-06-01 13:55:40 +0800

    update

--- personal-profile-webpage-master/jemdoc/makefile.bat
+++ personal-profile-webpage-master/jemdoc/makefile.bat
@@ -1,10 +1,10 @@
-python ./jemdoc.py -c custom.conf -o ../ index.jemdoc
-python ./jemdoc.py -o ../ biography.jemdoc
-python ./jemdoc.py -o ../ news.jemdoc
-python ./jemdoc.py -o ../ teaching.jemdoc
-python ./jemdoc.py -o ../ services.jemdoc
-python ./jemdoc.py -o ../ awards.jemdoc
-python ./jemdoc.py -o ../ contact.jemdoc
-python ../ref/bibconvert.py --suffix web --highlight "Y. Li" --highlight "Yongfu Li" --highlight "Li, Yongfu" --input ../ref/Top.bib --input ../ref/publications.bib --header publications_header.jemdoc > publications.jemdoc
-python ./jemdoc.py  -o ../ publications.jemdoc
+python3 ./jemdoc.py -c custom.conf -o ../ index.jemdoc
+python3 ./jemdoc.py -o ../ biography.jemdoc
+python3 ./jemdoc.py -o ../ news.jemdoc
+python3 ./jemdoc.py -o ../ teaching.jemdoc
+python3 ./jemdoc.py -o ../ services.jemdoc
+python3 ./jemdoc.py -o ../ awards.jemdoc
+python3 ./jemdoc.py -o ../ contact.jemdoc
+python3 ../ref/bibconvert.py --suffix web --highlight "Y. Li" --highlight "Yongfu Li" --highlight "Li, Yongfu" --input ../ref/Top.bib --input ../ref/publications.bib --header publications_header.jemdoc > publications.jemdoc
+python3 ./jemdoc.py  -o ../ publications.jemdoc
 pause
